79d08795f46d1749ec6f7aad1398beaa1c7bfd4c
Admin <Admin@IT21-NAFIZ>
2018-02-15 19:29:35 +0600

    bhfdg

---
+++ WebApplication1/Providers/ChallengeResult.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Net.Http;
+using System.Net;
+
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime;
+using System.Runtime.InteropServices;
+
+namespace WebApplication1.Providers
+{
+    public class ChallengeResult : IHttpActionResult
+    {
+        public ChallengeResult(string loginProvider, ApiController controller)
+        {
+            LoginProvider = loginProvider;
+            Request = controller.Request;
+        }
+
+        public string LoginProvider { get; set; }
+        public HttpRequestMessage Request { get; set; }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            Request.GetOwinContext().Authentication.Challenge(LoginProvider);
+
+            var response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            response.RequestMessage = Request;
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(WebApplication1.Startup))]
+
+namespace WebApplication1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            ConfigureAuth(app);
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+    }
+}
